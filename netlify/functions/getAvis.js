49b5eda877ae4ca77e5f0be3c9566be336debe8c
Francis <lalannefrancis431@gmail.com>
2026-03-10 14:43:38 +0100

    Debug getAvis

--- netlify/functions/getAvis.js
+++ netlify/functions/getAvis.js
@@ -1,23 +1,18 @@
 export async function handler() {
   try {
     const token = process.env.NETLIFY_TOKEN;
-    const siteId = process.env.SITE_ID;
 
-    const submissionsRes = await fetch(
-      `https://api.netlify.com/api/v1/sites/${siteId}/forms/avis/submissions?access_token=${token}`
+    const res = await fetch(
+      `https://api.netlify.com/api/v1/sites/f5631fa2-0720-48be-8bd8-a26c47e228c3/forms/avis/submissions?access_token=${token}`
     );
 
-    const submissions = await submissionsRes.json();
-
-    const formatted = submissions.map(s => ({
-      nom: s.data.nom,
-      message: s.data.message
-    }));
+    const text = await res.text(); // read raw response
 
     return {
       statusCode: 200,
-      body: JSON.stringify(formatted)
+      body: text
     };
+
   } catch (err) {
     return {
       statusCode: 500,
