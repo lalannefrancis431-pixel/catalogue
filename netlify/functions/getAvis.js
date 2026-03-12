717e44d9df5764e4d9a82f0649447b259a0ec893
Francis <lalannefrancis431@gmail.com>
2026-03-12 13:57:07 +0100

    Test getAvis function

--- netlify/functions/getAvis.js
+++ netlify/functions/getAvis.js
@@ -1,29 +1,9 @@
 export async function handler() {
-  try {
-    const res = await fetch("https://sound-industry.netlify.app/.netlify/functions/forms", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        action: "list-submissions",
-        formName: "avis"
-      })
-    });
-
-    const data = await res.json();
-
-    const formatted = data.submissions.map(s => ({
-      nom: s.data.nom,
-      message: s.data.message
-    }));
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(formatted)
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      tokenExists: !!process.env.NETLIFY_TOKEN,
+      tokenLength: process.env.NETLIFY_TOKEN ? process.env.NETLIFY_TOKEN.length : 0
+    })
+  };
 }
